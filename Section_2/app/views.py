abff08d1542b759ae24808ac0613d3b8922cb8ef
NoorMhd7 <146771080+NoorMhd7@users.noreply.github.com>
2024-10-20 19:39:11 +0000

    Section 4 done

--- Section_2/app/views.py
+++ Section_2/app/views.py
@@ -1,7 +1,12 @@
-from flask import render_template
+from flask import render_template, flash
 from app import app
+from .forms import CalculatorForm
 
-@app.route('/')
-def displayFruit():
-    fruits = ["Apple", "Banana", "Orange", "Kiwi"]
-    return render_template("inheritance.html",fruits=fruits)
+@app.route('/', methods=['GET', 'POST'])
+def calculator():
+    form = CalculatorForm()
+    if form.validate_on_submit():
+        flash('Succesfully received form data. %s + %s  = %s'%(form.number1.data, form.number2.data, form.number1.data+form.number2.data))
+    return render_template('calculator.html',
+                           title='Calculator',
+                           form=form)
