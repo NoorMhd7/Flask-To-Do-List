a966c85e830f8f3d2112911c2b06fac6a9784f4f
NoorMhd7 <146771080+NoorMhd7@users.noreply.github.com>
2024-11-29 13:39:08 +0000

    sec 8

--- section7_startpoint/app/views.py
+++ section7_startpoint/app/views.py
@@ -1,6 +1,12 @@
-from app import app
+from app import app, db, admin
+from flask_admin.contrib.sqla import ModelView
+from .models import Property, Landlord
+
+admin.add_view(ModelView(Property, db.session))
+admin.add_view(ModelView(Landlord, db.session))
 
 @app.route('/')
 def index():
-    return "Hello World!!!"
+    return "Hello!!!"
+
 
